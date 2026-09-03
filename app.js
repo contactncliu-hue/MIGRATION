bd7d35d5302eb8db75c80370209a4232e3a3f240
contactncliu-hue <contact.ncliu@gmail.com>
2026-09-04 02:51:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ---- Supabase setup ----
-const SUPABASE_URL = "YOUR_SUPABASE_URL"
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY"
+const SUPABASE_URL = "https://nlqzanjivjfkftvazrvi.supabase.co"
+const SUPABASE_ANON_KEY = "sb_publishable_M3WiVkgt6yctuilI5Q0IjQ_djrAu238"
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 // ---- Element references ----
