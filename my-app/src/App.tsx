82b91bc00e8ea22907a3a00638c7cc3f9b00f31f
contactncliu-hue <contact.ncliu@gmail.com>
2026-09-12 14:46:30 +0800

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { RequireAdmin } from './components/RequireAdmin'
+import { AuthProvider } from './lib/auth-context'
 import { HomePage } from './pages/HomePage'
 import { TransferPage } from './pages/TransferPage'
 import { LoginPage } from './pages/LoginPage'
@@ -8,21 +9,23 @@ import { AdminPage } from './pages/AdminPage'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/transfer" element={<TransferPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/admin"
-          element={
-            <RequireAdmin>
-              <AdminPage />
-            </RequireAdmin>
-          }
-        />
-      </Routes>
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/transfer" element={<TransferPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/admin"
+            element={
+              <RequireAdmin>
+                <AdminPage />
+              </RequireAdmin>
+            }
+          />
+        </Routes>
+      </Layout>
+    </AuthProvider>
   )
 }
 
