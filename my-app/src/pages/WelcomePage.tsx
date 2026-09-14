13a476ba067e83e47914257d22d0f2d4ddf3324f
contactncliu-hue <contact.ncliu@gmail.com>
2026-09-14 10:42:39 +0800

    Update WelcomePage.tsx

--- my-app/src/pages/WelcomePage.tsx
+++ my-app/src/pages/WelcomePage.tsx
@@ -14,30 +14,30 @@ export function WelcomePage() {
         Schedule
       </button>
 
-      <div className="welcome-brand">
-        <img className="welcome-logo" src="/assets/logo.png.PNG" alt="zOo" />
-        <img className="welcome-flower" src="/assets/flower.PNG" alt="" />
+      <div className="welcome-layout">
+        <div className="welcome-left">
+          <img className="welcome-feature" src="/assets/panel1.png" alt="" />
+          <img className="welcome-title" src="/assets/title-red.PNG" alt="zOo" />
+          <img className="welcome-branches" src="/assets/flower.PNG" alt="" />
+        </div>
+
+        <div className="welcome-panels">
+          <div className="welcome-panel">
+            <img className="welcome-panel-bg" src="/assets/panel2.png" alt="" />
+            <img className="welcome-panel-icon" src="/assets/scroll.png" alt="Scroll" />
+          </div>
+          <div className="welcome-panel">
+            <img className="welcome-panel-bg" src="/assets/panel3.png" alt="" />
+            <img className="welcome-panel-icon" src="/assets/sword.png" alt="Sword" />
+          </div>
+          <div className="welcome-panel">
+            <img className="welcome-panel-bg" src="/assets/panel4.png" alt="" />
+            <img className="welcome-panel-icon" src="/assets/quest.png" alt="Mystery" />
+          </div>
+        </div>
       </div>
 
-      <div className="welcome-panels">
-        <img className="welcome-panel welcome-panel-feature" src="/assets/panel1.png" alt="" />
-
-        <div className="welcome-panel welcome-panel-frame">
-          <img className="welcome-panel-bg" src="/assets/panel2.png" alt="" />
-          <img className="welcome-panel-icon" src="/assets/scroll.png" alt="Scroll" />
-        </div>
-
-        <div className="welcome-panel welcome-panel-frame">
-          <img className="welcome-panel-bg" src="/assets/panel3.png" alt="" />
-          <img className="welcome-panel-icon" src="/assets/sword.png" alt="Sword" />
-        </div>
-
-        <div className="welcome-panel welcome-panel-frame">
-          <img className="welcome-panel-bg" src="/assets/panel4.png" alt="" />
-          <img className="welcome-panel-icon" src="/assets/quest.png" alt="Mystery" />
-        </div>
-      </div>
-
+      <img className="welcome-line" src="/assets/Line.PNG" alt="" />
       <div className="welcome-caption">WELCOME TO ZO.O</div>
     </div>
   )
